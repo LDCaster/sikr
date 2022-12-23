570bac49a9166f23f278e992c2beda188b6888a1
AbdulGhoniZaen <abdulzaen13@gmail.com>
2022-12-23 09:31:57 +0800

    update ke-3 (rapikan sidebar)

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -11,73 +11,73 @@
                    <ul class="metismenu" id="menu1">
                        <li class="active">
                            <a class="" href="/dashboard">
-                               <i class="fa-solid fa-house-chimney"></i>
-                               <span class="mini-click-non">Dashboard</span>
+                               <span class="mini-click-non"><i
+                                       class="fa-solid fa-house-chimney"></i>&ensp;Dashboard</span>
                            </a>
                        </li>
                        <li>
                            <a title="Data Kontrak" href="/kontrak" aria-expanded="false">
-                               <i class="fa-regular fa-calendar-days" aria-hidden="true"></i>
-                               <span class="mini-click-non">Data Kontrak</span></a>
+                               <span class="mini-click-non"><i class="fa-regular fa-calendar-days"
+                                       aria-hidden="true"></i>&ensp;&nbsp;Data Kontrak</span></a>
                        </li>
                        <li>
                            <a title="Data Pabrikan" href="/pabrikan" aria-expanded="false">
-                               <i class="fa-regular fa-calendar-days" aria-hidden="true"></i>
-                               <span class="mini-click-non">Data Pabrikan</span></a>
+                               <i class="fa-solid fa-city" aria-hidden="true"></i>
+                               <span class="mini-click-non">&nbsp;Data Pabrikan</span></a>
                        </li>
                        <li>
-                           <a class="has-arrow" href="" aria-expanded="false"><span
-                                   class="educate-icon educate-professor icon-wrap"></span> <span
-                                   class="mini-click-non">Data Variant</span></a>
+                           <a class="has-arrow" href="" aria-expanded="false">
+                               <i class="fa-solid fa-coins"></i>
+                               <span class="mini-click-non">&nbsp;&nbsp;Data Variant</span></a>
                            <ul class="submenu-angle" aria-expanded="false">
                                <li>
                                    <a title="Variant" href="/variant">
-                                       <span class="mini-sub-pro">Variant</span>
+                                       <span class="mini-sub-pro">&nbsp;Variant</span>
                                    </a>
                                </li>
                                <li>
                                    <a title="Jenis Variant" href="/jenis-variant">
-                                       <span class="mini-sub-pro">Jenis Variant</span>
+                                       <span class="mini-sub-pro">&nbsp;Jenis Variant</span>
                                    </a>
                                </li>
                            </ul>
                        </li>
                        <li>
                            <a title="Data Pengadaan" href="/pengadaan" aria-expanded="false">
-                               <i class="fa-regular fa-calendar-days" aria-hidden="true"></i>
-                               <span class="mini-click-non">Data Pengadaan</span></a>
+                               <i class="fa-solid fa-note-sticky" aria-hidden="true"></i>
+                               <span class="mini-click-non">&ensp;Data Pengadaan</span></a>
                        </li>
                        <li>
                            <a title="Data Direksi Pekerjaan" href="/direksi-pekerjaan" aria-expanded="false">
-                               <i class="fa-regular fa-calendar-days" aria-hidden="true"></i>
-                               <span class="mini-click-non">Data Direksi Pekerjaan</span></a>
+                               <i class="fa-solid fa-users-between-lines"aria-hidden="true"> </i>
+                               <span class="mini-click-non">&nbsp;Data Direksi Pekerjaan</span></a>
                        </li>
                        <li>
-                           <a class="has-arrow" href="" aria-expanded="false"><span
-                                   class="educate-icon educate-professor icon-wrap"></span> <span
-                                   class="mini-click-non">Data Material</span></a>
+                           <a class="has-arrow" href="" aria-expanded="false">
+                               <i class="fa-solid fa-shapes"></i>
+                               <span class="mini-click-non">&nbsp;&nbsp;Data Material</span></a>
                            <ul class="submenu-angle" aria-expanded="false">
                                <li>
                                    <a title="Material" href="/material">
-                                       <span class="mini-sub-pro">Material</span>
+                                       <span class="mini-sub-pro">&nbsp;Material</span>
                                    </a>
                                </li>
                                <li>
                                    <a title="Jenis Material" href="jenis-material">
-                                       <span class="mini-sub-pro">Jenis Material</span>
+                                       <span class="mini-sub-pro">&nbsp;Jenis Material</span>
                                    </a>
                                </li>
                            </ul>
                        </li>
                        <li>
                            <a title="Data Satuan" href="/satuan" aria-expanded="false">
-                               <i class="fa-regular fa-calendar-days" aria-hidden="true"></i>
-                               <span class="mini-click-non">Data Satuan</span></a>
+                               <i class="fa-solid fa-s" aria-hidden="true"></i>
+                               <span class="mini-click-non">&ensp;&nbsp;Data Satuan</span></a>
                        </li>
                        <li>
                            <a title="Data PRK" href="/prk" aria-expanded="false">
-                               <i class="fa-regular fa-calendar-days" aria-hidden="true"></i>
-                               <span class="mini-click-non">Data PRK</span></a>
+                               <i class="fa-solid fa-newspaper" aria-hidden="true"></i>
+                               <span class="mini-click-non">&ensp;Data PRK</span></a>
                        </li>
                        <li>
                            <a class="has-arrow" href="" aria-expanded="false"><span
